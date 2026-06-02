2cc6ab3936fe2a89666cbed35922091520bb3da9
Sabel <info+1@sabelcustomersuccess.com>
2026-06-02 19:21:20 +1000

    Add Hero Tolk tracker key

--- api/tracker.js
+++ api/tracker.js
@@ -15,6 +15,7 @@ const ALLOWED_KEYS = [
   'donkey-republic-tracker-v1',
   'bts-tracker-v1',
   'aftership-tracker-v1',
+  'hero-tolk-tracker-v1',
   'bts-usa-june-tracker-v1',
   'bts-usa-tracker-v1',
   'flkitover-tracker-v1',
