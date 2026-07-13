fab6ee6b645f1e211b7ac96d4fe03582049d9480
adminsabel <admin@sabelcustomersuccess.com>
2026-07-13 11:09:14 +0800

    Add 'soundingboard-tracker-v1' to tracker list

--- api/tracker.js
+++ api/tracker.js
@@ -36,6 +36,7 @@ const ALLOWED_KEYS = [
   'sabel-golive-overrides-v1',
   'positive-salary-packaging-tracker-v1',
   'sabel-weekly-updates-v1',
+  'soundingboard-tracker-v1',
 ];
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
