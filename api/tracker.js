e14e9aee639105983de97021e8735c9fbc42a34b
adminsabel <admin@sabelcustomersuccess.com>
2026-08-03 16:24:54 +0800

    Include 'sabel-hours-v1' in tracker list
    Add internal hours tracker to the list of trackers.

--- api/tracker.js
+++ api/tracker.js
@@ -39,6 +39,7 @@ const ALLOWED_KEYS = [
   'soundingboard-tracker-v1',
   'sabel-client-info-v1',
   'eugenelabs-tracker-v1',
+  'sabel-hours-v1',                  // ← internal hours tracker + menu bar widget
 ];
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
