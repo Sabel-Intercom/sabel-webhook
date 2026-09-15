897d6b3c255a0879943d87d51cfe9f31247e786c
adminsabel <admin@sabelcustomersuccess.com>
2026-09-15 21:07:55 +0800

    Add migration intake shiftmove tracker to array

--- api/tracker.js
+++ api/tracker.js
@@ -52,6 +52,7 @@ const ALLOWED_KEYS = [
   'migration-intake-clevercards-v1',
   'migration-intake-soundingboard-v1',
   'released-remediation-tracker-v1',
+  'migration-intake-shiftmove-v1',
 ];
 
 // -------------------------------------------------------------
