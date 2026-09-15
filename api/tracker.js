66e387bd116135e274d6ade7bf68bc472a3465fd
adminsabel <admin@sabelcustomersuccess.com>
2026-09-15 23:25:51 +0800

    Add readiness-inputs-v1 to tracker

--- api/tracker.js
+++ api/tracker.js
@@ -53,6 +53,7 @@ const ALLOWED_KEYS = [
   'migration-intake-soundingboard-v1',
   'released-remediation-tracker-v1',
   'migration-intake-shiftmove-v1',
+  "readiness-inputs-v1",
 ];
 
 // -------------------------------------------------------------
